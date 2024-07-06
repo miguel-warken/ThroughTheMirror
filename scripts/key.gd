29566546243f23e1510bebac37f676f1130e307f
GustavoWMSilva <willian.gustavo@edu.pucrs.br>
2024-07-06 20:40:34 -0300

    key -> Corrigido

--- scripts/key.gd
+++ scripts/key.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 func _on_key_body_entered(body):
-	if body.is_in_group("Escalador") and body.keyCollected == true:
+	if body.is_in_group("Escalador"):
+		body.keyCollected = true
 		print(body.keyCollected)
 		queue_free()
